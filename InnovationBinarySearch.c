ebc68ceb1ae33d89313294c8f6b52f5777398934
TejasNere99 <tejasnere94@gmail.com>
2025-03-07 14:23:53 +0530

    Add files via upload

---
+++ InnovationBinarySearch.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("ENTER THE SIZE OF THE ARRAY : ");
+    scanf("%d",&n);
+    int arr[n],target,count=0;
+    printf("PLEASE ENTER THE %d ELEMENTS OF ARRAY :\n",n);
+    for(int i=0;i<n;i++){
+        printf("ELEMENT NO.%d : ",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("ENTER YOUR TARGET : ");
+    scanf("%d",&target);
+    int start=0,end=n-1,mid;
+    while(start<=end){
+        mid = (start+end)/2;
+        if(arr[mid]==target){
+           printf("It is at index : ");
+           int left=mid-1,right=mid+1;
+           while(arr[left]==arr[mid]&&left>=0){
+            printf("%d ",left);
+            left--;
+           }
+           printf("%d ",mid);
+           while(arr[right]==arr[mid]&&right<n){
+            printf("%d ",right);
+            right++;
+           }
+            count++;
+            break;
+        }else if(arr[mid]<target){
+            start=mid+1;
+        }else{
+            end = mid-1;
+        }
+    }
+    if(count==0){
+        printf("Not found");
+    }
+
+    return 0;
+}
